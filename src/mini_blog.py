eadf4f2ea8a87db6a0830e6984bd8a5623a39457
PrplFox <foxedex29@gmail.com>
2024-01-31 21:58:57 +0100

    bootstrap implemented + complete change

--- src/mini_blog.py
+++ src/mini_blog.py
@@ -20,4 +20,4 @@ def page_not_found(_):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
-  miniblog.run(host='0.0.0.0', port=8080)
+  miniblog.run(host='0.0.0.0', port=8080, debug=True)
